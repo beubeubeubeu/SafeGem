cd01007d11d88bebf64f47aee357e934cf3782af
Beubeu <nguyen.hl.b@gmail.com>
2024-04-06 00:51:59 +0200

    add: visual on homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,22 @@
 import Link from 'next/link';
-import { Heading, Box, Text, Button } from '@chakra-ui/react';
+import { Heading, Box, Text, Button, Flex, VStack, Image } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <>
       <Box textAlign={'center'} mt={24}>
-        <Heading as='h1' size='4xl'>Your tickets into NFT collections</Heading>
-        <Link href='/collections'>
-          <Button colorScheme='teal' size={'lg'} mt={'48px'}>Start Collecting Now</Button>
-        </Link>
-        <Text mt={'24px'}>🎟️ 🎫 🧾</Text>
+        <Flex direction={{ base: 'column', md: 'row' }} justify="center" align="center">
+          <VStack flex="2" spacing="24px">
+            <Heading as='h1' size='4xl'>Your tickets into NFT collections</Heading>
+            <Link href='/collections' style={{ textDecoration: 'none' }}>
+              <Button colorScheme='teal' size={'lg'}>Start Collecting Now</Button>
+            </Link>
+            <Text>🎟️ 🎫 🧾</Text>
+          </VStack>
+          <Box flex="1" mt={{ base: '24px', md: '0' }} bgColor={"red"} minH={"314px"} minW={{ base: 'full', md: '314px' }}>
+            <Image src="/images/mosaiq.webp" alt="Your NFT Collection"/>
+          </Box>
+        </Flex>
       </Box>
     </>
   );
