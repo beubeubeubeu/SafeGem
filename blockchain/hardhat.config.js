974012d02558fb54d14db472296595190573ec32
Beubeu <nguyen.hl.b@gmail.com>
2024-04-05 16:14:58 +0200

    fix: hardhat test gh workflow 5

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -12,14 +12,14 @@ const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
 module.exports = {
   solidity: "0.8.25",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
     sepolia: {
       url: ALCHEMY,
       accounts: [`0x${WALLET_PK}`],
       chainId: 11155111
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
     }
   },
   etherscan: {
