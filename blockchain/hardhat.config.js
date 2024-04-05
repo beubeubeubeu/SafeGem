74c1fa152d11b17eb2115634175c377a49bbb556
Beubeu <nguyen.hl.b@gmail.com>
2024-04-05 17:12:08 +0200

    add: slither gh action 4

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -4,10 +4,10 @@ require('hardhat-docgen');
 require('dotenv').config()
 
 // Wallet PK exported from metamask
-const WALLET_PK = process.env.WALLET_PK || "3HVBO5BJie9Z2dsbEGDIyNjteJsefCT2ycMBZna0SCT5XkHPuLsuwmEwFEjfr0JZ";
-const ALCHEMY = process.env.ALCHEMY_URL || "https://eth-sepolia.g.alchemy.com/v2/drxiWcZkhqopfTB5hGeHEvhxJyaS4nGN";
+const WALLET_PK = process.env.WALLET_PK || "";
+const ALCHEMY = process.env.ALCHEMY_URL || "";
 // const INFURA_URL = process.env.INFURA_URL || "";
-const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "75W3K3QY1OT5DVFV9TFAHMQLFZ352N842E";
+const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
   solidity: "0.8.25",
@@ -20,7 +20,7 @@ module.exports = {
       url: ALCHEMY,
       accounts: [`0x${WALLET_PK}`],
       chainId: 11155111
-    }
+    },
   },
   etherscan: {
     apiKey: {
