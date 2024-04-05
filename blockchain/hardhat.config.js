d769885f60872c278e9e3c799e797296d4a06665
Beubeu <nguyen.hl.b@gmail.com>
2024-04-05 18:03:47 +0200

    fix: github action 16

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -9,27 +9,40 @@ const ALCHEMY = process.env.ALCHEMY_URL || "";
 // const INFURA_URL = process.env.INFURA_URL || "";
 const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
 
-console.log("WALLET_PK", WALLET_PK);
+if(process.env.LOCAL) {
+  module.exports = {
+    solidity: "0.8.25",
+    networks: {
+      hardhat: {
 
-module.exports = {
-  solidity: "0.8.25",
-  networks: {
-    hardhat: {
-
+      },
+      localhost: {
+        url: "http://127.0.0.1:8545",
+        chainId: 31337,
+      },
+      sepolia: {
+        url: ALCHEMY,
+        accounts: [`0x${WALLET_PK}`],
+        chainId: 11155111
+      },
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
-    },
-    sepolia: {
-      url: ALCHEMY,
-      accounts: [`0x${WALLET_PK}`],
-      chainId: 11155111
-    },
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN
+    etherscan: {
+      apiKey: {
+        sepolia: ETHERSCAN
+      }
     }
-  }
-};
+  };
+} else {
+  module.exports = {
+    solidity: "0.8.25",
+    networks: {
+      hardhat: {
+
+      },
+      localhost: {
+        url: "http://127.0.0.1:8545",
+        chainId: 31337,
+      },
+    }
+  };
+}
