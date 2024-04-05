c9c2eebccd56fb5230d67b72529cd461dac1206c
Beubeu <nguyen.hl.b@gmail.com>
2024-04-05 17:15:46 +0200

    fix: github action 5

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -12,6 +12,9 @@ const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
 module.exports = {
   solidity: "0.8.25",
   networks: {
+    hardhat: {
+
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
